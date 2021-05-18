8026be0f12dcc517285b86169baec348a6a92801
flyinglove <446419676@qq.com>
2021-05-18 23:48:13 +0800

    feat: home页面

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -23,42 +23,56 @@
           </ul>
         </div>
 
-        <div class="article-preview">
+        <div class="article-preview" v-for="article in articles" :key="article.slug">
           <div class="article-meta">
-            <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
+            <!-- <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a> -->
+            <nuxt-link :to="{
+                name: 'profile',
+                params: {
+                    username: article.author.username
+                }
+            }"><img :src="article.author.image" /></nuxt-link>
             <div class="info">
-              <a href="" class="author">Eric Simons</a>
-              <span class="date">January 20th</span>
+              <!-- <a href="" class="author">Eric Simons</a> -->
+              <nuxt-link :to="{
+                name: 'profile',
+                params: {
+                    username: article.author.username
+                }
+            }" class="author">{{article.author.username}}</nuxt-link>
+              <span class="date">{{article.createdAt}}</span>
             </div>
-            <button class="btn btn-outline-primary btn-sm pull-xs-right">
-              <i class="ion-heart"></i> 29
+            <button class="btn btn-outline-primary btn-sm pull-xs-right" :class="{
+                active: article.favorited
+            }">
+              <i class="ion-heart"></i> {{article.favoritesCount}}
             </button>
           </div>
-          <a href="" class="preview-link">
-            <h1>How to build webapps that scale</h1>
-            <p>This is the description for the post.</p>
+          <nuxt-link :to="{
+            name: 'article',
+            params: {
+                slug: article.slug
+            }
+          }" class="preview-link">
+            <h1>{{article.title}}</h1>
+            <p>{{article.description}}</p>
             <span>Read more...</span>
-          </a>
+          </nuxt-link>
         </div>
+        <nav>
+            <ul class="pagination">
 
-        <div class="article-preview">
-          <div class="article-meta">
-            <a href="profile.html"><img src="http://i.imgur.com/N4VcUeJ.jpg" /></a>
-            <div class="info">
-              <a href="" class="author">Albert Pai</a>
-              <span class="date">January 20th</span>
-            </div>
-            <button class="btn btn-outline-primary btn-sm pull-xs-right">
-              <i class="ion-heart"></i> 32
-            </button>
-          </div>
-          <a href="" class="preview-link">
-            <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
-            <p>This is the description for the post.</p>
-            <span>Read more...</span>
-          </a>
-        </div>
+                <li class="page-item" :class="{active: page === item}" v-for="item in totalPage" :key="item">
+                    <nuxt-link class="page-link" :to="{
+                        name: 'home',
+                        query: {
+                            page: item
+                        }
+                    }">{{item}}</nuxt-link>
+                </li>
 
+            </ul>
+        </nav>
       </div>
 
       <div class="col-md-3">
@@ -85,8 +99,26 @@
 </template>
 
 <script>
+import { getArticles } from '@/api/article'
 export default {
-    name: 'HomeIndex'
+    name: 'HomeIndex',
+    watchQuery: ['page'],
+    async asyncData({query}) {
+        const page = Number.parseInt(query.page || 1)
+        const limit = 20
+        const { data } = await getArticles({
+            limit,
+            offset: (page - 1) * limit
+        })
+        const totalPage = Math.ceil(data.articlesCount / limit)
+        return {
+            articles: data.articles,
+            articleCount: data.articlesCount,
+            page,
+            limit,
+            totalPage
+        }
+    }
 }
 </script>
 
